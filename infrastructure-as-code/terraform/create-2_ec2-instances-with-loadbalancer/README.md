da6f79d57c9fad8e83d2768a39785eb1f0cb0221
tanakaryo <yoichiikegawa@Yoichi-no-MacBook-Pro.local>
2023-09-05 04:14:54 +0900

    #5 add ALB resource.

---
+++ infrastructure-as-code/terraform/create-2_ec2-instances-with-loadbalancer/README.md
@@ -0,0 +1,6 @@
+# Creating 2 EC2 instances with loadbalancer
+ - provider: aws
+ - tasks: create ec2-with-loadbalancer using terraform
+ - resource: ec2, loadbalancer
+
+## Architecture
---
+++ infrastructure-as-code/terraform/create-2_ec2-instances-with-loadbalancer/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "ap-northeast-1"
+}
+
+variable "az_1" {
+    default = "ap-northeast-1a"
+}
+
+variable "az_2" {
+    default = "ap-northeast-1c"
+}
